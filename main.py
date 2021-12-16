2e8d5c46d9a02f4f83cd57617a1ccacf70b667fd
zhaopengfei <hbyx.zpf@gmail.com>
2021-12-16 12:13:01 +0800

    update

--- main.py
+++ main.py
@@ -5,29 +5,29 @@ import requests, time, datetime, re,sys, json, random
 # 用户名（格式为 13800138000）
 
 # 酷推skey和server酱sckey和企业微信设置，只用填一个其它留空即可
-skey = sys.argv[3]
+skey = 'NO' #sys.argv[3]
 # 推送server酱
-sckey = sys.argv[4]
+sckey = 'NO' #sys.argv[4]
 # 企业微信推送
 # 是否开启企业微信推送false关闭true开启，默认关闭，开启后请填写设置并将上面两个都留空
-position = sys.argv[5]
+position = False #sys.argv[5]
 base_url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?'
 req_url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token='
-corpid = sys.argv[6]  # 企业ID， 登陆企业微信，在我的企业-->企业信息里查看
-corpsecret = sys.argv[7]  # 自建应用，每个自建应用里都有单独的secret
-agentid = sys.argv[8]  # 填写你的应用ID，不加引号，是个整型常数,就是AgentId
-touser = sys.argv[9]  # 指定接收消息的成员，成员ID列表（多个接收者用‘|’分隔，最多支持1000个）。特殊情况：指定为”@all”，则向该企业应用的全部成员发送
-toparty = sys.argv[10]  # 指定接收消息的部门，部门ID列表，多个接收者用‘|’分隔，最多支持100个。当touser为”@all”时忽略本参数
-totag = sys.argv[11]  # 指定接收消息的标签，标签ID列表，多个接收者用‘|’分隔，最多支持100个。当touser为”@all”时忽略本参数
+corpid = ''#sys.argv[6]  # 企业ID， 登陆企业微信，在我的企业-->企业信息里查看
+corpsecret = '' #sys.argv[7]  # 自建应用，每个自建应用里都有单独的secret
+agentid = '' #sys.argv[8]  # 填写你的应用ID，不加引号，是个整型常数,就是AgentId
+touser = '' #sys.argv[9]  # 指定接收消息的成员，成员ID列表（多个接收者用‘|’分隔，最多支持1000个）。特殊情况：指定为”@all”，则向该企业应用的全部成员发送
+toparty = '' #sys.argv[10]  # 指定接收消息的部门，部门ID列表，多个接收者用‘|’分隔，最多支持100个。当touser为”@all”时忽略本参数
+totag = '' #sys.argv[11]  # 指定接收消息的标签，标签ID列表，多个接收者用‘|’分隔，最多支持100个。当touser为”@all”时忽略本参数
 
 # （用于测试推送如果改了能收到推送，推送设置就没问题，看看是不是set_push列表里面没设置推送，仔细看下面我写的很详细）要修改的步数，直接输入想要修改的步数值，（默认）留空为随机步数，改了这个直接运行固定值（用于测试推送）
 # 测试好记得留空不然一直提交固定步数
-step1 = ""
+step1 = "66666"
 
 # 开启根据地区天气情况降低步数（默认关闭）
-open_get_weather = sys.argv[12]
+open_get_weather =True #sys.argv[12]
 # 设置获取天气的地区（上面开启后必填）如：area = "宁波"
-area = sys.argv[13]
+area = '北京' #sys.argv[13]
 
 # 以下如果看不懂直接默认就行只需改上面
 
